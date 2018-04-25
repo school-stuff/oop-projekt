329d3ac8584b48de8c53b76f2bcd257cf9a97102
TaliKert <32733365+TaliKert@users.noreply.github.com>
2018-04-25 17:20:46 +0300

    Application close on window close. (#33)

--- client/src/main/java/Scenes/Render.java
+++ client/src/main/java/Scenes/Render.java
@@ -1,8 +1,11 @@
 package Scenes;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -21,6 +24,13 @@ public class Render extends Application {
         primaryStage.setHeight(575);
         primaryStage.setWidth(575);
         primaryStage.setResizable(false);
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
 
         new LoginAndRegisterScene();
 
