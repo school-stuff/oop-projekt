b11428a773942f6072c7c2cf917ccedae65a380e
Agnes Saik <agnes.saik@gmail.com>
2018-05-16 21:45:39 +0300

    made it prettier

--- client/src/main/java/scenes/ImageOpener.java
+++ client/src/main/java/scenes/ImageOpener.java
@@ -2,26 +2,23 @@ package scenes;
 
 import javafx.scene.image.Image;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ImageOpener {
     private static Map<String, Image> images = new HashMap<>();
-    static {
-        String[] fileNames = {"images/character.png", "images/grass.png", "images/rock.png", "images/water.png", "images/wall.png"};
-        for (String name : fileNames) {
-            images.put(name, null);
-        }
-    }
+    private static List<String> fileNames = Arrays.asList("character.png", "grass.png", "rock.png", "water.png", "wall.png");
 
     public static Image getImage(String imageType) {
-        String filename = "images/" + imageType + ".png";
-        if (!images.containsKey(filename)) {
-            throw new RuntimeException("Imagefile " + filename + " not found!");
+        String filename = imageType + ".png";
+        if (!fileNames.contains(filename)) {
+            throw new RuntimeException("Imagefile image/" + filename + " not found!");
         }
 
-        if (images.get(filename) == null) {
-            Image image = new Image(filename);
+        if (!images.containsKey(filename)) {
+            Image image = new Image("images/" + filename);
             images.put(filename, image);
             return image;
         }
